109b2a41a317cc6049a3e305101ed8ba2aa9fecc
siouxsiexd <happyvicky2712@gmail.com>
2019-02-22 21:45:39 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import GlobalStyle from './styles/global';
+import { Container, Content } from './styles';
+
+import Upload from './components/Upload';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Upload />
+        </Content>
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
+
+export default App;
